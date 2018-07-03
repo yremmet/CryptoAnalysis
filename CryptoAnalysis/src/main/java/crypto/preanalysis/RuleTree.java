6ea55a5dd2d63b7acebc3c7bf3f90a0b4b3604bb
Rajiv Thorat <rajivthorat2003@gmail.com>
2018-07-03 10:04:47 +0200

    Fix for concurrent access of List

--- CryptoAnalysis/src/main/java/crypto/preanalysis/RuleTree.java
+++ CryptoAnalysis/src/main/java/crypto/preanalysis/RuleTree.java
@@ -15,9 +15,13 @@ public class RuleTree {
     private List<TreeNode<TreeNodeData>> listOfTreeNodes;
     private TreeNode<TreeNodeData> newObjectNode;
 
+    private Map<TreeNode<TreeNodeData>,TreeNode<TreeNodeData>> toBeSwitched; // <tobeRemoved, toBeAdded>, use the parent of toBeRemoved.
+    private Map<TreeNode<TreeNodeData>,TreeNode<TreeNodeData>> toBeInserted; // <parent, child>
+
 
     public RuleTree(List<ClassSpecification> specifications) {
         treeNodes = new ArrayList<>();
+        toBeSwitched = new HashMap<>();
         for (ClassSpecification specification : specifications) {
             treeNodes.add(new TreeNode(specification));
             if (Utils.getFullyQualifiedName(specification.getRule()).equals("java.lang.Object")){
@@ -92,6 +96,8 @@ public class RuleTree {
     public RuleTree(List<ClassSpecification> specifications, boolean psuedo) {
 
         listOfTreeNodes = new ArrayList<>();
+        toBeSwitched = new HashMap<>();
+        toBeInserted = new HashMap<>();
         for (ClassSpecification specification : specifications) {
             //SootClass classFromClassSpecification = Scene.v().getSootClass(Utils.getFullyQualifiedName(specification.getRule()));
             SootClass classFromClassSpecification = Scene.v().forceResolve(Utils.getFullyQualifiedName(specification.getRule()),SootClass.HIERARCHY);
@@ -111,10 +117,28 @@ public class RuleTree {
 
         for (TreeNode<TreeNodeData> treeNode : listOfTreeNodes) {
             newInsertNode(treeNode,newRuleHeir);
+            //  The nodes that need to be switched. The iterative call to the insertNode method is readonly.
+            postInsertion();
         }
 
         // change the return statement;
         return newRuleHeir;
+    }
+
+    public void postInsertion(){
+        if (toBeSwitched != null){
+            for (Map.Entry<TreeNode<TreeNodeData>, TreeNode<TreeNodeData>> treeNodeTreeNodeEntry : toBeSwitched.entrySet()) {
+                treeNodeTreeNodeEntry.getKey().parent.addChild(treeNodeTreeNodeEntry.getValue().data);
+                treeNodeTreeNodeEntry.getKey().parent.children.remove(treeNodeTreeNodeEntry.getKey());
+            }
+            toBeSwitched.clear();
+        }
+        if (toBeInserted != null){
+            for (Map.Entry<TreeNode<TreeNodeData>, TreeNode<TreeNodeData>> treeNodeTreeNodeEntry : toBeInserted.entrySet()) {
+                treeNodeTreeNodeEntry.getKey().addChild(treeNodeTreeNodeEntry.getValue().data);
+            }
+            toBeInserted.clear();
+        }
     }
 
     //recursive
@@ -126,27 +150,46 @@ public class RuleTree {
             System.out.println(rootNode.data.getSootClass().getName() + "\n");
                 if (Scene.v().getOrMakeFastHierarchy().isSubclass(nodeUnderConsideration.data.getSootClass(), rootNode.data.getSootClass())){
 
-                    for (TreeNode<TreeNodeData> child : rootNode.children) {
+                    /*for(Iterator<TreeNode<TreeNodeData>> iterator = rootNode.children.iterator(); iterator.hasNext();){
+                        TreeNode<TreeNodeData> child = iterator.next();
                         successfulInsertion = newInsertNode(nodeUnderConsideration, child);
+                    }*/
+                    for (TreeNode<TreeNodeData> child : rootNode.children) {
+                        successfulInsertion = newInsertNode(nodeUnderConsideration, child) | successfulInsertion;
                     }
+
                     // after traversal through the children, if insertion is unsuccessful, or if there are no children, add the node as a child.
                     if(!successfulInsertion){
-                        rootNode.addChild(nodeUnderConsideration.data);
-                        successfulInsertion = true;
+                        if (!rootNode.children.contains(nodeUnderConsideration.data)){
+                            rootNode.addChild(nodeUnderConsideration.data);
+                            successfulInsertion = true;
+                        }
+
                     }
 
                 }// if the above check is not true, check if the hierarchy is the other way around.
                 else if(Scene.v().getOrMakeFastHierarchy().isSubclass(rootNode.data.getSootClass(), nodeUnderConsideration.data.getSootClass())){
                     // Update the parent of the switched node.
-                    rootNode.parent.addChild(nodeUnderConsideration.data);
-                    rootNode.parent.children.remove(rootNode);
+
+                    toBeSwitched.put(rootNode,nodeUnderConsideration);
+//                    rootNode.parent.addChild(nodeUnderConsideration.data);
+//                    rootNode.parent.children.remove(rootNode);
+
+                    /*for(Iterator<TreeNode<TreeNodeData>> iterator = nodeUnderConsideration.children.iterator();iterator.hasNext();){
+                        TreeNode<TreeNodeData> child = iterator.next();
+                        successfulInsertion = newInsertNode(child, nodeUnderConsideration);
+                    }*/
 
                     for (TreeNode<TreeNodeData> child : nodeUnderConsideration.children) {
-                        successfulInsertion = newInsertNode(child, nodeUnderConsideration);
+                        successfulInsertion = newInsertNode(child, nodeUnderConsideration) | successfulInsertion;
                     }
                     if(!successfulInsertion){
-                        nodeUnderConsideration.addChild(rootNode.data);
-                        successfulInsertion = true;
+                        if (!nodeUnderConsideration.children.contains(rootNode.data)){
+                            //nodeUnderConsideration.addChild(rootNode.data);
+                            toBeInserted.put(nodeUnderConsideration,rootNode);
+                            successfulInsertion = true;
+                        }
+
                     }
 
                 } else {
