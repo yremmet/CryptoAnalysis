15d6996c3c5438d24ecff9d7ee0834121ac265bd
Rajiv Thorat <rajivthorat2003@gmail.com>
2018-07-18 15:14:05 +0200

    Got rid of unnecessary code

--- CryptoAnalysis/src/main/java/crypto/preanalysis/TreeNode.java
+++ CryptoAnalysis/src/main/java/crypto/preanalysis/TreeNode.java
@@ -1,13 +1,9 @@
 package crypto.preanalysis;
-/**
- * Source : https://github.com/gt4dev/yet-another-tree-structure
- */
 
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
-public class TreeNode<T> implements Iterable<TreeNode<T>> {
+public class TreeNode<T>{
 
     public T data;
     public TreeNode<T> parent;
@@ -21,55 +17,16 @@ public class TreeNode<T> implements Iterable<TreeNode<T>> {
         return children.size() == 0;
     }
 
-    private List<TreeNode<T>> elementsIndex;
-
     public TreeNode(T data) {
         this.data = data;
-        this.children = new LinkedList<TreeNode<T>>();
-        this.elementsIndex = new LinkedList<TreeNode<T>>();
-        this.elementsIndex.add(this);
+        this.children = new ArrayList<>();
     }
 
     public TreeNode<T> addChild(T child) {
         TreeNode<T> childNode = new TreeNode<T>(child);
         childNode.parent = this;
         this.children.add(childNode);
-        this.registerChildForSearch(childNode);
         return childNode;
-    }
-
-    public int getLevel() {
-        if (this.isRoot())
-            return 0;
-        else
-            return parent.getLevel() + 1;
-    }
-
-    private void registerChildForSearch(TreeNode<T> node) {
-        elementsIndex.add(node);
-        if (parent != null)
-            parent.registerChildForSearch(node);
-    }
-
-    public TreeNode<T> findTreeNode(Comparable<T> cmp) {
-        for (TreeNode<T> element : this.elementsIndex) {
-            T elData = element.data;
-            if (cmp.compareTo(elData) == 0)
-                return element;
-        }
-
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        return data != null ? data.toString() : "[data null]";
-    }
-
-    @Override
-    public Iterator<TreeNode<T>> iterator() {
-        TreeNodeIter<T> iter = new TreeNodeIter<T>(this);
-        return iter;
     }
 
 }
